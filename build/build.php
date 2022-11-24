603c0be2466139a269d9a94c961643763d4c78d5
SharkyKZ <sharkykz@gmail.com>
2022-11-24 14:26:05 +0200

    Update build script

--- build/build.php
+++ build/build.php
@@ -2,19 +2,8 @@
 
 require (dirname(__DIR__)) . '/build-script/script.php';
 
-$plugin = dirname(__DIR__) . '/code/plugins/system/emailverification/emailverification.php';
-$script = dirname(__DIR__) . '/code/media/plg_system_emailverification/js/script.js';
-
-$hash = substr(hash_file('sha1', $script, false), 0, 8);
-$code = file_get_contents($plugin);
-
-$pattern = '/(private\s+static\s+\$scriptHash\s+=\s+\')(.*)(\';)/';
-$code = preg_replace($pattern, '${1}' . $hash . '$3', $code);
-
-file_put_contents($plugin, $code);
-
 (
-	new PluginBuildScript(
+	new class(
 		str_replace('\\', '/', dirname(__DIR__)),
 		str_replace('\\', '/', __DIR__),
 		'emailverification',
@@ -25,5 +14,22 @@ file_put_contents($plugin, $code);
 		'Plugin for verifying email addresses before registration.',
 		'(4\.|3\.([89]|10))',
 		'5.3.10',
-	)
+	) extends PluginBuildScript
+	{
+		public function build(): void
+		{
+			$plugin = $this->pluginDirectory . '/' . $this->pluginElement . '.php';
+			$script = $this->mediaDirectory . '/js/script.js';
+
+			$hash = substr(hash_file('sha1', $script, false), 0, 8);
+			$code = file_get_contents($plugin);
+
+			$pattern = '/(private\s+static\s+\$scriptHash\s+=\s+\')(.*)(\';)/';
+			$code = preg_replace($pattern, '${1}' . $hash . '$3', $code);
+
+			file_put_contents($plugin, $code);
+
+			parent::build();
+		}
+	}
 )->build();
